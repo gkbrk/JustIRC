18382cab4f07c7f33ca8c04e4e0337828266b018
Gökberk Yaltıraklı <webdosusb@gmail.com>
2015-03-22 20:39:55 +0200

    Added new example weatherbot.py.

---
+++ examples/weatherbot.py
@@ -0,0 +1,32 @@
+import SimpleIRC
+import requests
+
+bot = SimpleIRC.IRCConnection()
+
+def on_connect(bot):
+    bot.set_nick("WeatherBot")
+    bot.send_user_packet("WeatherBot")
+
+def on_welcome(bot):
+    bot.join_channel("#WeatherBotTest")
+
+def on_message(bot, channel, sender, message):
+    if len(message.split()) == 0:
+        message = "." #For people who try to crash bots with just spaces
+
+    if message.split()[0] == "!weather":
+        if len(message.split()) > 1:
+            weather_data = requests.get("http://api.openweathermap.org/data/2.5/weather", params={"q": message.split(" ", 1), "units": "metric"}).json()
+            if weather_data["cod"] == 200:
+                bot.send_message(channel, "The weather in {} is {} and {} degrees.".format(weather_data["name"], weather_data["weather"][0]["description"], weather_data["main"]["temp"]))
+        else:
+            bot.send_message(channel, "Usage: !weather Istanbul")
+    elif message.split()[0] == "!test":
+        bot.send_message(channel, "Test successful!")
+
+bot.on_connect.append(on_connect)
+bot.on_welcome.append(on_welcome)
+bot.on_public_message.append(on_message)
+
+bot.connect("irc.freenode.net")
+bot.run_loop()
