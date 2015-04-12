4aaa8798aa3decadaac85c7a42e9c96c55d69f55
Gökberk Yaltıraklı <webdosusb@gmail.com>
2015-04-12 22:33:50 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = "SimpleIRC",
+    py_modules = ["SimpleIRC"],
+    version = "0.1",
+    description = "An event-driven IRC library",
+    author = "Gokberk Yaltirakli",
+    author_email = "webdosusb@gmail.com",
+    url = "https://github.com/gkbrk/SimpleIRC",
+    download_url = "https://github.com/gkbrk/SimpleIRC/tarball/master"
+    keywords = ["irc", "event"]
+)
