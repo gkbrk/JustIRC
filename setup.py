5a59eee28426a00dd9d35451860e1be43c79b521
Gökberk Yaltıraklı <webdosusb@gmail.com>
2015-04-12 22:57:43 +0300

    Renamed SimpleIRC to JustIRC in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 setup(
-    name = "SimpleIRC",
-    py_modules = ["SimpleIRC"],
+    name = "JustIRC",
+    py_modules = ["JustIRC"],
     version = "0.1",
     description = "An event-driven IRC library",
     author = "Gokberk Yaltirakli",
     author_email = "webdosusb@gmail.com",
-    url = "https://github.com/gkbrk/SimpleIRC",
-    download_url = "https://github.com/gkbrk/SimpleIRC/tarball/master",
+    url = "https://github.com/gkbrk/JustIRC",
+    download_url = "https://github.com/gkbrk/JustIRC/tarball/master",
     keywords = ["irc", "event"]
 )
