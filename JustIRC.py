a6344b4553aae70a073fe530556310cf779550d3
Gökberk Yaltıraklı <webdosusb@gmail.com>
2019-01-12 17:58:30 +0000

    doc: Add docstring documentation to public methods

--- JustIRC.py
+++ JustIRC.py
@@ -1,12 +1,12 @@
 import socket
 import time
 
-def parse_irc_packet(packet):
-    irc_packet = IRCPacket()
+def _parse_irc_packet(packet):
+    irc_packet = _IRCPacket()
     irc_packet.parse(packet)
     return irc_packet
 
-class IRCPacket:
+class _IRCPacket:
     def __init__(self):
         self.prefix = ""
         self.command = ""
@@ -38,6 +38,10 @@ class IRCPacket:
 
 class IRCConnection:
     def __init__(self):
+        """Creates a new IRC Connection. You need to call .connect on it to actually
+        connect to a server.
+
+        """
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         self.nick = ""
@@ -52,7 +56,12 @@ class IRCConnection:
         self.on_leave = []
 
     def run_once(self):
-        packet = parse_irc_packet(next(self.lines)) #Get next line from generator
+        """This function runs one iteration of the IRC client. This is called in a loop
+        by the run_loop function. It can be called separately, but most of the
+        time there is no need to do this.
+
+        """
+        packet = _parse_irc_packet(next(self.lines)) #Get next line from generator
 
         for event_handler in list(self.on_packet_received):
             event_handler(self, packet)
@@ -85,10 +94,14 @@ class IRCConnection:
                 event_handler(self, packet.arguments[0], packet.prefix.split("!")[0])
 
     def run_loop(self):
+        """Runs the main loop of the client. This function is usually called after you
+        add all the callbacks and connect to the server.
+
+        """
         while True:
             self.run_once()
 
-    def read_lines(self):
+    def _read_lines(self):
         buff = ""
         while True:
             buff += self.socket.recv(1024).decode("utf-8", "replace")
@@ -98,29 +111,66 @@ class IRCConnection:
                 yield line
 
     def connect(self, server, port=6667):
+        """Connects to a given IRC server. After the connection is established, it calls
+        the on_connect event handler.
+
+        """
         self.socket.connect((server, port))
-        self.lines = self.read_lines()
+        self.lines = self._read_lines()
         for event_handler in list(self.on_connect):
             event_handler(self)
 
     def send_line(self, line):
+        """Sends a line directly to the server. This is a low-level function that can be
+        used to implement functionality that's not covered by this
+        library. Almost all of the time, you should have no need to use this
+        function.
+
+        """
         self.socket.send("{}\r\n".format(line).encode("utf-8"))
 
     def send_message(self, to, message):
+        """Sends a message to a user or a channel. This is the main way of interaction
+        as an IRC bot or client.
+
+        """
         self.send_line("PRIVMSG {} :{}".format(to, message))
 
     def send_notice(self, to, message):
+        """Sends a notice message. Notice messages ususally have special formatting on
+        clients.
+
+        """
         self.send_line("NOTICE {} :{}".format(to, message))
 
     def send_action_message(self, to, action):
+        """Sends an action message to a channel or user. Action messages have special
+        formatting on clients and are usually sent like /me is happy
+
+        """
         self.send_message(to, "\x01ACTION {}\x01".format(action))
 
     def join_channel(self, channel_name):
+        """Joins a given channel. After the channel is joined, the on_join callback is
+        called.
+
+        """
         self.send_line("JOIN {}".format(channel_name))
 
     def set_nick(self, nick):
+        """Sets or changes your link. This should be called before joining channels, but
+        can be called at any time afterwards. If the requested nickname is not
+        available, the library will keep adding an underscore until a suitable
+        nick is found.
+
+        """
         self.nick = nick
         self.send_line("NICK {}".format(nick))
 
     def send_user_packet(self, username):
+        """Sends a user packet. This should be sent after your nickname. It is
+        displayed on clients when they view your details and look at "Real
+        Name".
+
+        """
         self.send_line("USER {} 0 * :{}".format(username, username))
